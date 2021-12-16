bfb41d7230a28e3e2cd1c4afe8fc64b97dd61d4a
jacy <jacy_li@kingdee.com>
2021-12-16 17:44:08 +0800

    pod引入第三方库Sdk2

---
+++ NMannager.h
@@ -0,0 +1,16 @@
+//
+//  NMannager.h
+//  JDYYPodTest
+//
+//  Created by Jacy Lee on 2021/12/16.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NMannager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NMannager.m
@@ -0,0 +1,12 @@
+//
+//  NMannager.m
+//  JDYYPodTest
+//
+//  Created by Jacy Lee on 2021/12/16.
+//
+
+#import "NMannager.h"
+
+@implementation NMannager
+
+@end
